12afb613e095ee14f0215c903e54492496a15713
Theodore Smith <tjs7321@gmail.com>
2024-01-07 01:20:36 -0500

    added stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
+import { useEffect, useState } from "react";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushiList, setSushiList] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(r => setSushiList(r))
+  }, [])
+
   return (
     <div className="app">
       <SushiContainer />
